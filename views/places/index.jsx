b20567eb1375e4af30f3f56bf47d76e9670be6aa
CKline22 <CKline.1022@gmail.com>
2022-04-04 14:45:40 -0400

    3 part 6 adding array

---
+++ views/places/index.jsx
@@ -0,0 +1,15 @@
+//PLACES INDEX PAGE
+const React = require('react')
+const Def = require('../default')
+
+function index () {
+    return (
+      <Def>
+          <main>
+              <h1>places index</h1>
+          </main>
+      </Def>
+    )
+  }
+
+module.exports = index
